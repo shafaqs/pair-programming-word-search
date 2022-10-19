d05039488956a9f517162cf7941e8a0c901b221a
Shafaq Saud <shafaq.saud@gmail.com>
2022-10-19 13:28:33 -0400

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programming with @shafaqs & @jayrmesa
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = joinVertically(letters).map((ls) => ls.join(""));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    // false statement instead of undefined 
+    return false;
+};
+
+const joinVertically = (letters) => {
+    const newArr = [];
+
+    for (let row = 0; row < letters[0].length; row++) {
+        newArr.push([]);
+
+        for (let col = 0; col < letters[0].length; col++) {
+            newArr[row].push(letters[col][row]);
+        }
+
+
+    }
+    return newArr;
+};
+
+
+
+module.exports = wordSearch;
